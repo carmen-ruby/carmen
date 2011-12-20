f2ac65b378e529d0f4b9a367fa8383f1bc541eb5
Jim Benton <jim@autonomousmachine.com>
2011-12-20 16:35:07 -0600

    Add the ability to specify the data path.

--- lib/carmen.rb
+++ lib/carmen.rb
@@ -8,8 +8,42 @@ require 'carmen/country'
 
 module Carmen
   class << self
-    attr_accessor :data_path
+
+    # Public: Where the data files for Carmen are stored.
+    #
+    # Should be in the following format:
+    # |- world.yml (all countries)
+    # \- regions   (directory for subregions, named by code)
+    #  |- be.yml   (subregion file for a country)
+    #
+    # Defaults to the `iso_data` directory within the Carmen directory.
+    attr_reader :data_path
+
+    # Public: Where to store overlay data.
+    #
+    # Overlay data is loaded after the data in `data_path`. Entries that appear
+    # within the `overlay_path` take precedence.
+    #
+    # The files in this directory should be in an identical layout to those in
+    # `data_path`. There should only be a file in the overlay directory if the
+    # corresponding regoins has overlay data. Only the regions to be modified
+    # should be included in an overlay file.
+    #
+    # Defaults to nil (no overlay data).
+    attr_reader :overlay_path
+
+    # Public: Set the data path.
+    # path - The String path to the data directory.
+    def data_path=(path)
+      @data_path = Pathname.new(path)
+    end
+
+    # Public: Set the overlay data path.
+    # path - The String path to the overlay data directory.
+    def overlay_path=(path)
+      @overlay_path = Pathname.new(path)
+    end
   end
 
-  self.data_path = Pathname.new(File.expand_path('../../iso_data', __FILE__))
+  self.data_path = File.expand_path('../../iso_data', __FILE__)
 end
