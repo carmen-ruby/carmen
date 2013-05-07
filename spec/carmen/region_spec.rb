4d81fa8462be8b214d013bf1c36ef1391771da33
Jim Benton <jim@autonomousmachine.com>
2013-05-06 20:20:33 -0700

    Clean up the region_spec to use before blocks where appropriate.

--- spec/carmen/region_spec.rb
+++ spec/carmen/region_spec.rb
@@ -3,49 +3,52 @@
 require 'spec_helper'
 
 describe Carmen::Region do
-  before do
-    @airstrip_one = Carmen::Country.coded('OC').subregions.first
-  end
 
-  it 'has a reasonable inspect value' do
-    @airstrip_one.inspect.must_equal '<#Carmen::Region name="Airstrip One" type="province">'
-  end
-
-  it "has the correct subregion path" do
-    @airstrip_one.subregion_data_path.must_equal "world/oc/ao.yml"
-  end
-
-  it "knows if it has subregions" do
-    @airstrip_one.subregions?.must_equal true
-  end
-
-  it "has a path" do
-    @airstrip_one.path.must_equal 'world.oc.ao'
-  end
-
-  describe "subregions" do
+  describe 'basic methods' do
     before do
-      @london = @airstrip_one.subregions.first
+      @airstrip_one = Carmen::Country.coded('OC').subregions.first
     end
 
-    it "has a name" do
-      @london.name.must_equal "London"
+    it 'has a reasonable inspect value' do
+      @airstrip_one.inspect.must_equal '<#Carmen::Region name="Airstrip One" type="province">'
     end
 
-    it "has a code" do
-      @london.code.must_equal 'LO'
+    it "has the correct subregion path" do
+      @airstrip_one.subregion_data_path.must_equal "world/oc/ao.yml"
     end
 
-    it "has a type" do
-      @london.type.must_equal 'city'
+    it "knows if it has subregions" do
+      @airstrip_one.subregions?.must_equal true
     end
 
-    it "has a parent" do
-      @london.parent.must_equal @airstrip_one
+    it "has a path" do
+      @airstrip_one.path.must_equal 'world.oc.ao'
+    end
+
+    describe "subregions" do
+      before do
+        @london = @airstrip_one.subregions.first
+      end
+
+      it "has a name" do
+        @london.name.must_equal "London"
+      end
+
+      it "has a code" do
+        @london.code.must_equal 'LO'
+      end
+
+      it "has a type" do
+        @london.type.must_equal 'city'
+      end
+
+      it "has a parent" do
+        @london.parent.must_equal @airstrip_one
+      end
     end
   end
 
-  describe "Basic querying" do
+  describe "querying" do
     before do
       @world = Carmen::World.instance
     end
