4108acf99d5649f41274023b579fb0b5df31d70b
David Attilio Bellotti <dbellotti@gmail.com>
2013-10-28 16:14:51 -0700

    fix for issues 114 and 118: appended data paths for new regions causing error when loading non overlayed data

--- spec/carmen/overlay_spec.rb
+++ spec/carmen/overlay_spec.rb
@@ -16,6 +16,12 @@ describe "Data overlaying" do
     sealand.type.must_equal('fort')
   end
 
+  it 'verifies overlay exists in before loading yaml file' do
+    oceana = Carmen::Country.coded('OC')
+    oceana.instance_of?(Carmen::Country).must_equal true
+    oceana.type.must_equal('country')
+  end
+
   it 'removes elements that have _enabled set to false' do
     Carmen::World.instance.subregions.size.must_equal(3)
     Carmen::Country.named('Eurasia').must_equal nil
